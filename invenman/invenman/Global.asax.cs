2e68c4499e8e56a91c434014e69d08c7119901bd
oranegonzales <78328723+oranegonzales@users.noreply.github.com>
2026-07-22 10:58:33 -0500

    Harden authentication and authorization

--- invenman/invenman/Global.asax.cs
+++ invenman/invenman/Global.asax.cs
@@ -1,21 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
+using invenman.Security;
 
 namespace invenman
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        protected void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            HttpContext context = HttpContext.Current;
+            if (context == null || context.Session == null)
+            {
+                return;
+            }
+
+            AuthSecurity.TryRestoreSession(context);
+            if (AuthorizationRules.CanAccess(context))
+            {
+                return;
+            }
+
+            bool loggedIn =
+                !string.IsNullOrWhiteSpace(context.Session["Username"] as string);
+            context.Response.Redirect(
+                loggedIn ? "~/Home.aspx" : "~/Login.aspx",
+                false);
+            context.ApplicationInstance.CompleteRequest();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception error = Server.GetLastError();
+            if (error != null)
+            {
+                System.Diagnostics.Trace.TraceError(error.ToString());
+            }
         }
     }
 }
